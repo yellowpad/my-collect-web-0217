b1d743d5ee13994b74713a1977763c501d60cc3c
Zayne Abraham <zayneabraham1@gmail.com>
2017-02-01 02:03:14 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
 
+def my_collect(array)
+  if array.size == 0
+    return
 
+  else
+      empty = []
+      if array.include?"ruby"
+        array.each do |lang|
+          lang = lang.upcase
+          empty << lang
+        end
+
+      else
+        array.each do |lang|
+          lang =  lang.split(" ").first
+          lang.capitalize
+          empty << lang
+        end
+      end
+    return empty
+  end
+
+end
